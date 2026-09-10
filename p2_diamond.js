345b5342d229ba903eba7db2d5fcefcf586a080f
JunKimkw <kimjy519@kw.ac.kr>
2026-09-10 11:05:00 +0900

    p2: diamond

--- p2_diamond.js
+++ p2_diamond.js
@@ -10,7 +10,20 @@
 //   *
 
 const n = Number(process.argv[2] ?? 5);
-
 // TODO: print the diamond with for / if and console.log.
 // Hint: for row i, count the spaces and the stars separately.
 // Hint: "*".repeat(3) gives "***".
+
+if (n % 2 === 0) { //홀수만 가능
+    console.log("n must be odd");
+} else {
+    for (let i = 1; i <= (n + 1) / 2; i++) {
+        console.log(" ".repeat((n + 1) / 2 - i) + "*".repeat((i - 1) * 2 + 1));
+    }
+
+    for (let i = (n - 1) / 2; i > 0; i--) {
+        console.log(" ".repeat((n + 1) / 2 - i) + "*".repeat((i - 1) * 2 + 1));
+    }
+}
+// 2 1 0 1 2 공백
+// 1 3 5 3 1 별
