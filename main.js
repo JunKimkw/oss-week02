aace437e0b04589a576c59a74f7eb467b4ed3064
JunKimkw <kimjy519@kw.ac.kr>
2026-09-10 21:30:30 +0900

    p5: inko cli

--- main.js
+++ main.js
@@ -28,6 +28,8 @@
 import readline from "node:readline";
 
 // TODO: import Inko from "inko" and create an instance
+import Inko from "inko";
+const inko = new Inko();
 
 const reverse = process.argv.includes("--reverse");
 const lines = [];
@@ -53,4 +55,14 @@ rl.on("close", () => {
   //   1. 오픈 소스
   //   2. 깃허브 노드
   // Use ko2en instead of en2ko when reverse is true.
+  const changed = lines
+    .filter((line) => line.trim() !== "")
+    .map((line) => {
+      if (reverse) return inko.ko2en(line);
+      else return inko.en2ko(line);
+    });
+
+  changed.forEach((t, i) => {
+    console.log(i + 1 + ".", t);
+  });
 });
